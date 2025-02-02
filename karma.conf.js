c631418f44a39d0992100bc89b4a7767bf278c02
fareedmirzay <fareedmirzay@ymail.com>
2025-02-02 13:55:57 -0500

    new deployment3

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,13 +22,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    // Use ChromeHeadlessCI for CI and Chrome for local development
+    browsers: process.env.CI ? ['ChromeHeadlessCI'] : ['Chrome'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     },
+    // Optional: Increase timeout for CI environments
+    captureTimeout: 120000,  // Timeout for capturing the browser (2 minutes)
+    browserNoActivityTimeout: 120000,  // Timeout for no browser activity (2 minutes)
     singleRun: false,
     restartOnFileChange: true
   });
